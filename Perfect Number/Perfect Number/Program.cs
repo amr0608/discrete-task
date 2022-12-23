7d93a6451c6994ce7fbcda14d47fcba89c347a37
amr0608 <115639083+amr0608@users.noreply.github.com>
2022-12-24 01:04:10 +0200

    Add files via upload

---
+++ Perfect Number/Perfect Number/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perfect_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to count Perfect Number");
+            Console.WriteLine("please enter First Number");
+            int N1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("please enter Second Number");
+            int N2 = int.Parse(Console.ReadLine());
+            int i;
+            Console.WriteLine("the Numbers is :");
+            for (; N1 <= N2; N1++)
+            {
+                int sum = 0;
+                for ( i = 1; i < N1; i++)
+                {
+                    if (N1 % i == 0)
+                    {
+                        sum += i;
+                    }
+                }
+                if (sum == i)
+                {
+                    
+                    Console.WriteLine(i);
+                }
+            }
+ 
+        }
+    }
+}
---
+++ Prime Number/Prime Number/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.ExceptionServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loob
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter first number");
+            int FirstNumber =int.Parse(Console.ReadLine());
+
+            Console.WriteLine("please enter second number");
+            int SecondNumber = int.Parse(Console.ReadLine());
+            
+            int x;
+            
+            for (; FirstNumber<=SecondNumber; FirstNumber++)
+            {
+                for (x=FirstNumber;x<=FirstNumber;x++)
+                if (FirstNumber % 2 == 0)
+                    break;
+             else if (x == FirstNumber)
+                    Console.WriteLine(FirstNumber);
+            }
+        }
+    }
+}
